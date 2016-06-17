18ecf4e98554c5b6ec9a0035b4e396915483f8c6
emre.akkaya <emre.akkaya@agem.com.tr>
2016-06-17 10:47:42 +0300

    update debian conf

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -5,6 +5,7 @@
 #
 # Generated files can be found under /tmp/lider-ahenk-user-privilege-plugin/
 ###
+set -e
 
 pushd $(dirname $0) > /dev/null
 PRJ_ROOT_PATH=$(dirname $(pwd -P))
@@ -29,8 +30,8 @@ echo "Export path: $EXPORT_PATH"
 # Copy resulting files
 echo "Copying generated files to $EXPORT_PATH..."
 mkdir -p "$EXPORT_PATH"
-mv -f "$PRJ_ROOT_PATH"/user-privilege*.deb "$EXPORT_PATH"
-mv -f "$PRJ_ROOT_PATH"/user-privilege*.changes "$EXPORT_PATH"
+mv -f "$PRJ_ROOT_PATH"/*.deb "$EXPORT_PATH"
+mv -f "$PRJ_ROOT_PATH"/*.changes "$EXPORT_PATH"
 cp -rf "$PRJ_ROOT_PATH"/lider-user-privilege/target/lider-*.jar "$EXPORT_PATH"
 cp -rf "$PRJ_ROOT_PATH"/lider-console-user-privilege/target/lider-console-*.jar "$EXPORT_PATH"
 echo "Copied generated files."
