6397e471c5db444901c842e29492c559423da0bd
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-07 11:31:52 +0300

    main module info added

--- ahenk-user-privilege/user-privilege/main.py
+++ ahenk-user-privilege/user-privilege/main.py
@@ -3,4 +3,14 @@
 
 
 def info():
-    return None
+    inf = dict()
+    inf['name'] = 'user-privilege'
+    inf['version'] = '1.0.0'
+    inf['support'] = 'debian'
+    inf['description'] = ''
+    inf['task'] = False
+    inf['user_oriented'] = True
+    inf['machine_oriented'] = False
+    inf['developer'] = ''
+
+    return inf
